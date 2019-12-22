bba3e3883e8ed32169e7fdf1b3e5a4707dd046d0
Abhiram Shibu <abhiramshibu1998@gmail.com>
2019-12-22 11:14:43 +0530

    Improved Battery Driver

--- power.py
+++ power.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env pypy3
 import os
-class battery:
-    battery_data={}
-    path=""
+import math
+class power:
     def read_data(self):
-        standard_files=["type", "power_now", "capacity", "cycle_count", "voltage_now",
-                        "energy_now", "energy_full_design","energy_full"]
+        standard_files=["type", "power_now", "capacity", "capacity_level" , "cycle_count",  
+                        "voltage_now","energy_now", "energy_full_design","energy_full",
+                        "model_name"]
         for i in standard_files:
             current_file=self.path+"/"+i
             if(os.path.exists(current_file)):
@@ -14,17 +14,28 @@ class battery:
                 f.close()
                 self.battery_data[i]=data
             else:
-                print("Error no such file ",current_file)
+                print("Warning no such file ",current_file)
 
     def __init__(self,path):
+        self.battery_data={}
+        self.path=""
         self.path=path
         self.read_data()
     def __repr__(self):
-        return str(self.battery_data)
-
+        returnData=""
+        for i in self.battery_data:
+            length=len(i)
+            if(length<15):
+                spacing="\t"*2
+            if(length<10):
+                spacing="\t"*2
+            if(length<5):
+                spacing="\t"*3
+            returnData+=i+spacing+self.battery_data[i]+"\n"
+        return returnData
 
 power_supplies=os.listdir("/sys/class/power_supply")
-def get_batteries():
+def get_powersupplies():
     batteries=[]
     mains=[]
     others=[]
@@ -36,17 +47,22 @@ def get_batteries():
             contents=f.read().strip()
             f.close()
             if(contents=="Battery"):
+                print("Detected Battery in ",actual_path)
                 batteries.append(actual_path)
             elif(contents=="Mains"):
                 mains.append(actual_path)
             else:
                 others.append(actual_path)
     return batteries,mains,others
-batteries,mains,others=get_batteries()
+def get_objects():
+    return_var={}
+    return_var["batteries"]=get_battery_objects()
+    return return_var
+batteries,mains,others=get_powersupplies()
 def get_battery_objects():
     battery_objects=[]
     for i in batteries:
-        batt=battery(i)
+        batt=power(i)
         battery_objects.append(batt)
     return battery_objects
-print(get_battery_objects())
+print(get_objects())
